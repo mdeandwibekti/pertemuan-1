aa10fd735d7762ef3b956edb3bd28f85b8896a90
mdeandwibekti <mdeandwibekti@gmail.com>
2026-02-21 20:20:34 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<html lang="id">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pertemuan 1</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+
+        body {
+
+            background-color: #0c0c0c;
+
+        }
+
+    </style>
+
+</head>
+
+<body class="flex items-center justify-center min-h-screen text-white antialiased">
+
+
+
+    <div class="w-full max-w-4xl p-16 border border-zinc-800/50 rounded-xl bg-zinc-900/20 shadow-2xl">
+
+       
+
+        <div class="space-y-1">
+
+            <h1 class="text-base font-semibold text-zinc-100 tracking-tight">
+
+                Muhammad Dea Dwi Bekti
+
+            </h1>
+
+            <p class="text-sm text-zinc-500 font-medium">
+
+                20230140042
+
+            </p>
+
+        </div>
+
+
+
+        <div class="mt-8">
+
+            <a href="#" class="inline-block bg-white text-black text-xs px-5 py-2.5 rounded-md font-bold hover:bg-zinc-200 transition-all shadow-lg active:scale-95">
+
+                Modul Pertemuan 1
+
+            </a>
+
+        </div>
+
+       
+
+    </div>
+
+
+
+</body>
+
+</html>
